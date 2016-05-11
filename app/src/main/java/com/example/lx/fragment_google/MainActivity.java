190d4ad8d71d34dd246263b65afe9c04cf346122
Koala <nwulx@126.com>
2016-05-11 10:52:37 +0800

    fragment google

--- app/src/main/java/com/example/lx/fragment_google/MainActivity.java
+++ app/src/main/java/com/example/lx/fragment_google/MainActivity.java
@@ -1,9 +1,10 @@
 package com.example.lx.fragment_google;
 
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements HeadlinesFragment.OnHeadlineSelectedListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +21,25 @@ public class MainActivity extends AppCompatActivity {
 
             getFragmentManager().beginTransaction()
                     .add(R.id.fragment_container, firstFragment).commit();  //getFragmentManager for v7; getSupportFragmentManager for v4
-            
+        }
+    }
+
+    public void onArticleSelected(int position){
+
+        ArticleFragment articleFragment = (ArticleFragment)getFragmentManager()
+                .findFragmentById(R.id.article_fragment);
+
+        if (articleFragment != null){
+            articleFragment.updateArticleView(position);
+        }else{
+            ArticleFragment newFragment = new ArticleFragment();
+            Bundle args = new Bundle();
+            args.putInt(ArticleFragment.ARG_POSITION, position);
+            newFragment.setArguments(args);
+            FragmentTransaction transaction = getFragmentManager().beginTransaction();
+            transaction.replace(R.id.fragment_container, newFragment);
+            transaction.addToBackStack(null);
+            transaction.commit();
         }
     }
 }
